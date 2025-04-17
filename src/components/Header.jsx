1700058b6872a2aa4983aadd93675335d6248945
emb-mohaala <mohammad.alam@embitel.com>
2025-04-17 10:04:21 +0530

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { auth } from '../utils/firebase'
 import { addUser, removeUser } from '../utils/userSlice'
+import { LOGO } from '../utils/constants'
 
 const Header = () => {
   const dispatch = useDispatch()
@@ -19,7 +20,7 @@ const Header = () => {
   }
 
   useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
         const { uid, email, displayName, photoURL } = user
         dispatch(
@@ -36,13 +37,14 @@ const Header = () => {
         navigate('/')
       }
     })
+    return () => unsubscribe() // cleanup function to unsubscribe from listener when component unmounts
   }, [])
 
   return (
     <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
       <img
         className="w-44"
-        src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+        src={LOGO}
         alt="logo"
       />
       {user && (
--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,27 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// export default defineConfig({
+//   plugins: [
+//     // other Vite plugins
+//     react({
+//       babel: {
+//         plugins: [
+//           // other Babel plugins
+//           [
+//             '@locator/babel-jsx/dist',
+//             {
+//               env: 'development',
+//             },
+//           ],
+//         ],
+//       },
+//     }),
+//     tailwindcss(),
+//   ],
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
